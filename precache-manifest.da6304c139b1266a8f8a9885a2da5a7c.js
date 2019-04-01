e3a31713be100ec669965de0ac03e75d9a6f2a7c
ivandw <ivandw@gmail.com>
2019-04-01 15:19:53 -0300

    Add files via upload

---
+++ precache-manifest.da6304c139b1266a8f8a9885a2da5a7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "86eaa35c0e32fcd3fb81",
+    "url": "/static/js/main.86eaa35c.chunk.js"
+  },
+  {
+    "revision": "b39344e026d1035650ee",
+    "url": "/static/js/2.b39344e0.chunk.js"
+  },
+  {
+    "revision": "86eaa35c0e32fcd3fb81",
+    "url": "/static/css/main.211443ab.chunk.css"
+  },
+  {
+    "revision": "c46be6072e85876470955d51f435137a",
+    "url": "/index.html"
+  }
+];
